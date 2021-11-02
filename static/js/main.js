0f2e4f2ca2129a0311a8af068ff4cd99a2bb161c
ahmadichsanbaihaqi <ahmadichsanbaihaqi@gmail.com>
2021-11-03 05:23:43 +0700

    format doc

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,13 @@
-$(function() {
+$(function () {
 	'use strict';
 
-	
-  $('.form-control').on('input', function() {
-	//   var $field = $(this).closest('.form-group');
-	//   if (this.value) {
-	//     $field.addClass('field--not-empty');
-	//   } else {
-	//     $field.removeClass('field--not-empty');
-	//   }
+	$('.form-control').on('input', function () {
+		//   var $field = $(this).closest('.form-group');
+		//   if (this.value) {
+		//     $field.addClass('field--not-empty');
+		//   } else {
+		//     $field.removeClass('field--not-empty');
+		//   }
 	});
 
 });
